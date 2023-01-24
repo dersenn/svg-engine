80299c6a065734d9add807c2e576a77f78b74046
dersenn <67318888+dersenn@users.noreply.github.com>
2023-01-24 18:53:58 +0100

    getting there. now paths!

--- v2/js/engine.js
+++ v2/js/engine.js
@@ -1,27 +1,35 @@
 // 24-01-2023
 // Reset The Preset.
 
-// the main class
-
 class SVG {
   constructor(setup) {
     this.ns = 'http://www.w3.org/2000/svg'
     this.xl = 'http://www.w3.org/1999/xlink'
-    this.mime = { type: "image/svg+xml" }
+    this.mime = { type: 'image/svg+xml' }
     this.parent = setup.parent
     this.id = setup.id
     this.w = this.parent.clientWidth
     this.h = this.parent.clientHeight
     this.els = []
 
-    this.bg = '#ff0'
+    // not really working yet.
+    this.defaults()
 
     // initialize and push to dom on creation.
     this.init()
   }
 
+  defaults(fill = '#000', stroke = '#000', strokeW = 1) {
+    this.def = {
+      fill: fill,
+      stroke: stroke,
+      strokeW: strokeW
+    }
+  }
+
   init() {
     this.stage = document.createElementNS(this.ns, 'svg')
+    this.stage.setAttribute('id', this.id)
     this.stage.setAttribute('xmlns', this.ns)
     this.stage.setAttribute('xmlns:xlink', this.xl)
     this.stage.setAttribute('width', this.w)
@@ -32,26 +40,18 @@ class SVG {
   }
 
   save() {
-    const str = new XMLSerializer().serializeToString(this.stage);
-    const blob = new Blob([str], this.mime);
+    const str = new XMLSerializer().serializeToString(this.stage)
+    const blob = new Blob([str], this.mime)
 
     const link = document.createElement("a"),
-          time = Math.round(new Date().getTime() / 1000);
-    link.download = `${document.title}-${time}.svg`;
-    link.href = URL.createObjectURL(blob);
-    link.click();
-    URL.revokeObjectURL(link.href);  
+          time = Math.round(new Date().getTime() / 1000)
+    link.download = `${document.title}-${time}.svg`
+    link.href = URL.createObjectURL(blob)
+    link.click()
+    URL.revokeObjectURL(link.href)
   }
 
-  draw() {
-    for (let e = 0; e < this.els.length; e++) {
-      this.stage.append(this.els[e])
-    }
-  }
-
-
-
-  makeLine(a, b, stroke = 'transparent', strokeW = 1) {
+  makeLine(a, b, stroke = this.def.stroke, strokeW = this.def.strokeW) {
     let line = document.createElementNS(this.ns, 'line')
     line.setAttribute('x1', a.x)
     line.setAttribute('y1', a.y)
@@ -59,10 +59,12 @@ class SVG {
     line.setAttribute('y2', b.y)
     line.setAttribute('stroke', stroke)
     line.setAttribute('stroke-width', strokeW)
-    this.els.push(line)
+    // this.els.push(line)
+    this.stage.append(line)
+    return line
   }
 
-  makeCircle(c, r, fill = 'transparent', stroke = 'transparent', strokeW = 0) {
+  makeCircle(c, r, fill = this.def.fill, stroke = this.def.stroke, strokeW = this.def.strokeW) {
     let circle = document.createElementNS(this.ns, 'circle')
     circle.setAttribute('cx', c.x)
     circle.setAttribute('cy', c.y)
@@ -70,10 +72,12 @@ class SVG {
     circle.setAttribute('fill', fill)
     circle.setAttribute('stroke', stroke)
     circle.setAttribute('stroke-width', strokeW)
-    this.els.push(circle)
+    // this.els.push(circle)
+    this.stage.append(circle)
+    return circle
   }
 
-  makeEllipse(c, rx, ry, fill = 'transparent', stroke = 'transparent', strokeW = 0) {
+  makeEllipse(c, rx, ry, fill = this.def.fill, stroke = this.def.stroke, strokeW = this.def.strokeW) {
     let ellipse = document.createElementNS(this.ns, 'ellipse')
     ellipse.setAttribute('cx', c.x)
     ellipse.setAttribute('cy', c.y)
@@ -82,10 +86,12 @@ class SVG {
     ellipse.setAttribute('fill', fill)
     ellipse.setAttribute('stroke', stroke)
     ellipse.setAttribute('stroke-width', strokeW)
-    this.els.push(ellipse)
+    // this.els.push(ellipse)
+    this.stage.append(ellipse)
+    return ellipse
   }
 
-  makeRect(pt, w, h, fill = 'transparent', stroke = 'transparent', strokeW = 0) {
+  makeRect(pt, w, h, fill = 'transparent', stroke = this.def.stroke, strokeW = this.def.strokeW) {
     let rect = document.createElementNS(this.ns, 'rect')
     rect.setAttribute('x', pt.x)
     rect.setAttribute('y', pt.y)
@@ -94,63 +100,61 @@ class SVG {
     rect.setAttribute('fill', fill)
     rect.setAttribute('stroke', stroke)
     rect.setAttribute('stroke-width', strokeW)
-    this.els.push(rect)
+    // this.els.push(rect)
+    this.stage.append(rect)
+    return rect
   }
 
-  makePath(d, fill = 'transparent', stroke = 'transparent', strokeW = 0) {
+  makePath(d, fill = this.def.fill, stroke = this.def.stroke, strokeW = this.def.strokeW) {
     let path = document.createElementNS(this.ns, 'path')
-    path.setAttributeNS(null, 'd', d)
+    path.setAttribute('d', d)
     path.setAttribute('fill', fill)
     path.setAttribute('stroke', stroke)
     path.setAttribute('stroke-width', strokeW)
-    this.els.push(path)
+    // this.els.push(path)
+    this.stage.append(path)
+    return path
   }
 
-  // To Do:
-  // makeGroup() {}
+  // Hmm... Group only useful if i can set this as parent for other Elements. Maybe I still need to split this up a bit more... Maybe an Elements Class???
+
+  // makeGroup(id) {
+  //   let group = document.createElementNS(this.ns, 'g')
+  //   group.setAttribute('id', id)
+  //   this.stage.append(group)
+  //   return group
+  // }
 
 }
 
+class Path {
+  constructor() {
+
+  }
+
+  addPtCub() {
+
+  }
+  
+  toStr() {
+
+  }
+}
 
 
-// The bones of a grid thing. TopLeft and BottomRight as defining Pts.
 class Tile {
-  constructor(tl, br) {
-    this.tl = tl
-    this.bl = new Vec(tl.x, br.y)
-    this.br = br
-    this.tr = new Vec(br.x, tl.y)
-    this.c = tl.mid(br)
-
-    // What else? corners array?
-  }
-}
-
-
-// Probably should only have one class, either Pt{} or Vec{}.
-// They are mostly identical...
-
-
-class Pt {
-  constructor(x, y, z = 0) {
-    this.x = x
-    this.y = y
-    this.z = z
-  }
-
-  // op = other Point
-  add(op) {
-    let xn = this.x + op.x,
-        yn = this.y + op.y,
-        zn = this.z + op.z
-    return new Pt(xn, yn, zn)
-  }
-
-  sub(op) {
-    let xn = this.x - op.x, 
-        yn = this.y - op.y,
-        zn = this.z - op.z
-    return new Pt(xn, yn, zn)
+  constructor(p1, p2) {
+    this.p1 = p1
+    this.p2 = p2
+    this.xMin = Math.min(p1.x, p2.x)
+    this.xMax = Math.max(p1.x, p2.x)
+    this.yMin = Math.min(p1.y, p2.y)
+    this.yMax = Math.max(p1.y, p2.y)
+    this.pMin = new Vec(this.xMin, this.yMin)
+    this.pMax = new Vec(this.xMax, this.yMax)
+    this.w = Math.abs(this.p2.x - this.p1.x)
+    this.h = Math.abs(this.p2.y - this.p1.y)
+    this.c = new Vec(this.p1.x + ((this.p2.x - this.p1.x) /2), this.p1.y + ((this.p2.y - this.p1.y) /2))
   }
 }
 
@@ -161,14 +165,12 @@ class Vec {
     this.z = z
     this.m = Math.sqrt(this.x**2 + this.y**2 + this.z**2) // Magnitude
   }
-
   norm() {
     let xn = this.x / this.m,
         yn = this.y / this.m,
         zn = this.z / this.m
     return new Vec(xn, yn, zn)
   }
-
   // ov = other Vec
   add(ov) {
     let xn = this.x + ov.x,
@@ -176,60 +178,52 @@ class Vec {
         zn = this.z + ov.z
     return new Vec(xn, yn, zn)
   }
-
   sub(ov) {
     let xn = this.x - ov.x, 
         yn = this.y - ov.y,
         zn = this.z - ov.z
     return new Vec(xn, yn, zn)
   }
-
   cross(ov) {
     let xn = this.y * ov.z - this.z * ov.y,
         yn = this.z * ov.x - this.x * ov.z,
         zn = this.x * ov.y - this.y * ov.x
     return new Vec(xn, yn, zn)
   }
-
   dot(ov) {
     return this.x * ov.x + this.y * ov.y + this.z * ov.z
   }
-
+  ang(ov) {
+    return Math.acos( this.norm().dot(ov.norm()) / (this.norm().m * ov.norm().m) )
+  }
   lerp(ov, t) {
     let xn = (1 - t) * this.x + ov.x * t,
         yn = (1 - t) * this.y + ov.y * t,
         zn = (1 - t) * this.z + ov.z * t
     return new Vec(xn, yn, zn)
   }
-
   mid(ov) {
     let xn = (this.x + ov.x) / 2,
         yn = (this.y + ov.y) / 2,
         zn = (this.z + ov.z) / 2
     return new Vec(xn, yn, zn)
   }
-
 }
-
-
 
 
 /////// HELPER FUNCTIONS.
 
-// Make those shorter. Arrow functions?!
-// shorten to rand(), randInt() etc.
-
 // Add random w/ seed.
-function random() {
+function rand() {
   return Math.random()
 }
 
-function randomInt(min, max) {
+function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function coinToss(chance) {
-  if (random() <= chance / 100) {
+  if (Math.random() <= chance / 100) {
     return true
   }
 }
@@ -253,6 +247,14 @@ function lerp(a, b, t) {
   return new Vec(xn, yn, zn)
 }
 
+function dot(a, b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z
+}
+
+function ang(a, b) {
+  return Math.acos( dot(a.norm(), b.norm()) / (a.norm().m * b.norm().m) )
+}
+
 function rad(deg) {
   return deg * (Math.PI / 180)
 }
@@ -261,16 +263,13 @@ function deg(rad) {
   return rad / (Math.PI / 180)
 }
 
-
-
-
 // Divide length between to points. Returns intermediary Points. Not so sure.
 // Looks very end-heavy to me. not really random.
 // Maybe add a minimum Distance.
 function divLength(a, b, nSeg, t = 1/nSeg, outA = []) {
-  if (t == 'RAND') {
+  if (t === 'RAND') {
     for (let i = 0; i < nSeg-1; i++) {
-      t = random()
+      t = Math.random()
       a = a.lerp(b, t)
       outA.push(a)
     }
--- v2/js/sketch.js
+++ v2/js/sketch.js
@@ -1,149 +1,66 @@
 
 const setup = {
-      id: '1',
+      id: 'mySVG',
       parent: document.body,
       presAspect: 'none', // needs more work
 }
 
-let svg = new SVG(setup)
+const defaults = {
+      fill: '#0f0',
+      stroke: '#0f0',
+      strokeW: 5
+}
 
-let nRows = 2
-let nCols = 2
-let cellW = svg.w / nCols
-let cellH = svg.h / nRows
-
-let xOff,
-    yOff
-
-for (let x = 0; x < nCols; x++) {
-  xOff = x * cellW
-  for (let y = 0; y < nRows; y++) {
-    yOff = y * cellH
-    let pts = []
-
-    let tl = new Vec(xOff, yOff),
-        br = new Vec(xOff + cellW, yOff + cellH)
-
-    let tile = new Tile(tl, br)
-
-    // svg.makeCircle(tile.tl, 5, 'rgb(0,255,0')
-    // svg.makeCircle(tile.bl, 5, 'rgb(0,255,0')
-    // svg.makeCircle(tile.br, 5, 'rgb(0,255,0')
-    // svg.makeCircle(tile.tr, 5, 'rgb(0,255,0')
-
-    // svg.makeCircle(tile.c, 5, 'rgb(0,0,255')
-
-    pts = pts.concat(
-          divLength(tile.tl, tile.bl, randomInt(2,4), 'RAND'),
-          divLength(tile.bl, tile.br, randomInt(2,4), 'RAND'),
-          divLength(tile.br, tile.tr, randomInt(2,4), 'RAND'),
-          divLength(tile.tr, tile.tl, randomInt(2,4), 'RAND')
-    )
+let svg = new SVG(setup, defaults)
 
 
-    // hmm, this????
-    pts = pts.concat(pts[0])
 
-    let d = ''
-    for (let j = 0; j < pts.length; j++) {
-      // console.log(j, pts.length)
 
-      switch (j) {
-        case 0:
-          d += `M ${pts[j].x} ${pts[j].y} `
-          break
-        // case pts.length-1: {
-        //   let m = pts[0].mid(pts[j])
-        //   let cp = m.lerp(tile.c, .5)
-        //   d += `Q ${cp.x} ${cp.y} ${pts[j].x} ${pts[j].y} `
-        //   break
-        // }
-        default: {
-          let m = pts[j].mid(pts[j-1])
-          let cp = m.lerp(tile.c, .5)
-          d += `Q ${cp.x} ${cp.y} ${pts[j].x} ${pts[j].y} `
-          break
-        }
-      }
-      // console.log(pts.length, pts[-1])
 
-    }
 
-    // console.log(d)
-    svg.makePath(d, '#0f0', 'transparent', 1)
+let nPts = 3
+let r = rand() * (svg.w/nPts)
 
-    // for (let i = 0; i < pts.length; i++) {
-    //   let p = nPts[i].mid(nPts[i+1])
-    //   svg.makeCircle(nPts[i], 5, '#f00')
-    //   svg.makeCircle(p, 3, '#0f0')
-    // }
-  }
+let a = new Vec(0, svg.h)
+let b = new Vec(svg.w, 0)
+
+let pts = divLength(a, b, 4)
+
+let crcls = []
+
+for (let i = 0; i < pts.length; i++) {
+  let crcl = svg.makeCircle(pts[i], r)
+  crcl.id = 'blah' + i
+  crcls.push(crcl)
 }
 
 
-// TESTS AND MATH
-
-// let p1 = new Vec(0, svg.h/2)
-// let p2 = new Vec(svg.w, svg.h/2)
-
-// svg.makeLine(p1, p2, '#f00')
-
-// let lrPts = divLength(p1, p2, 4, 'RAND')
+console.log(svg.stage)
+console.log(svg)
 
 
-// for (let i = 0; i < lrPts.length; i++) {
-//   svg.makeCircle(lrPts[i], 5, `rgb(${255/(i+1)}, 0, 0)`)
+// console.log(svg.els)
+
+// svg.draw()
+
+// function animate(t) {
+//   crcls[1].setAttribute('r', randInt(5, 10))
+//   requestAnimationFrame(animate)
 // }
 
-// console.log(lrPts)
+// animate(0)
+
+// const keyHandlerSave = (event) => {
+//   if (event.key === 's') {
+      //   svg.save()
+//         crcls[1].setAttribute('r', randInt(5, 10))
+//         console.log(crcls[1])
+//   }
+// }
+    
+// document.addEventListener('keypress', keyHandlerSave);
 
 
-
-///////////////////////////////////////////
-
-// let p1 = new Vec(0, 200)
-// let p2 = new Vec(100, 500)
-// let p3 = new Vec(400, 200)
-
-// let p12 = p1.mid(p2)
-// let p23 = p2.mid(p3)
-// let p31 = p3.mid(p1)
-
-
-// svg.makeCircle(p1, 5, '#ff0')
-// svg.makeCircle(p2, 5, '#ff0')
-// svg.makeCircle(p3, 5, '#ff0')
-
-// svg.makeCircle(p12, 5, '#f00')
-// svg.makeCircle(p23, 5, '#f00')
-// svg.makeCircle(p31, 5, '#f00')
-
-// svg.makeLine(p12, p3, '#0f0', 2)
-
-// let r = random()
-// let px = (1 - r) * p12.x + p3.x * r
-// let py = (1 - r) * p12.y + p3.y * r
-// let p = new Vec(px, py)
-// let pl = p12.lerp(p3, r)
-
-// svg.makeCircle(p, 5, '#f00')
-// svg.makeCircle(p, 10, '#f00', '#fff', 2)
-
-///////////////////////////////////////////
-
-svg.draw()
-
-
-
-// Save
-
-const keyHandlerSave = (event) => {
-  if (event.key === "s") {
-    svg.save()
-  }
-};
-
-document.addEventListener("keypress", keyHandlerSave);
 
 
 
